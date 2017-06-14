2235677a3ffab1971c30f12c1d19ca049d965b2d
heyuanlong <1023417614@qq.com>
2017-06-14 17:20:45 +0800

    dsfsfs

---
+++ __lua/syntaxTest/serialize.lua
@@ -0,0 +1,117 @@
+
+local utils = {}
+
+function utils.serialize(obj)  
+    local lua = ""  
+    local t = type(obj)  
+    if t == "number" then  
+        lua = lua .. obj  
+    elseif t == "boolean" then  
+        lua = lua .. tostring(obj)  
+    elseif t == "string" then  
+        lua = lua .. string.format("%q", obj)  
+    elseif t == "table" then  
+        lua = lua .. "{\n"  
+    for k, v in pairs(obj) do  
+        lua = lua .. "[" .. utils.serialize(k) .. "]=" .. utils.serialize(v) .. ",\n"  
+    end  
+    local metatable = getmetatable(obj)  
+        if metatable ~= nil and type(metatable.__index) == "table" then  
+        for k, v in pairs(metatable.__index) do  
+            lua = lua .. "[" .. utils.serialize(k) .. "]=" .. utils.serialize(v) .. ",\n"  
+        end  
+    end  
+        lua = lua .. "}"  
+    elseif t == "nil" then  
+        return nil  
+    else  
+        error("can not serialize a " .. t .. " type.")  
+    end  
+    return lua  
+end  
+  
+function utils.unserialize(lua)  
+    local t = type(lua)  
+    if t == "nil" or lua == "" then  
+        return nil  
+    elseif t == "number" or t == "string" or t == "boolean" then  
+        lua = tostring(lua)  
+    else  
+        error("can not unserialize a " .. t .. " type.")  
+    end  
+    lua = "return " .. lua  
+    local func = loadstring(lua)  
+    if func == nil then  
+        return nil  
+    end  
+    return func()  
+end  
+
+
+function utils.dumpObj(t)
+
+    local getIndent, quoteStr, wrapKey, wrapVal, dumpObj
+    getIndent = function(level)
+        return string.rep("\t", level)
+    end
+    quoteStr = function(str)
+        return '"' .. string.gsub(str, '"', '\\"') .. '"'
+    end
+    wrapKey = function(val)
+        if type(val) == "number" then
+            return "[" .. val .. "]"
+        elseif type(val) == "string" then
+            return "[" .. quoteStr(val) .. "]"
+        else
+            return "[" .. tostring(val) .. "]"
+        end
+    end
+    wrapVal = function(val, level)
+        if type(val) == "table" then
+            return dumpObj(val, level)
+        elseif type(val) == "number" then
+            return val
+        elseif type(val) == "string" then
+            return quoteStr(val)
+        else
+            return tostring(val)
+        end
+    end
+    dumpObj = function(obj, level)
+        if type(obj) ~= "table" then
+            return wrapVal(obj)
+        end
+        level = level + 1
+        local tokens = {}
+        tokens[#tokens + 1] = "{"
+        for k, v in pairs(obj) do
+            tokens[#tokens + 1] = getIndent(level) .. wrapKey(k) .. " = " .. wrapVal(v, level) .. ","
+        end
+        tokens[#tokens + 1] = getIndent(level - 1) .. "}"
+        return table.concat(tokens, "\n")
+    end
+    return dumpObj(t, 0)
+end
+
+
+
+local t = {}
+t.key={a=1,b=2}
+t.key2=2
+t.key3='dsfsdfsdfsdfdaaaaaaa"dddd"d'
+
+local str1 = utils.serialize(t)
+local str2 = utils.dumpObj(t)
+print(str1)
+print()
+print(str2)
+
+local lua1 = utils.unserialize(str1)
+for k,v in pairs(lua1) do
+    print(k,v)
+end
+print()
+local lua2 = utils.unserialize(str2)
+for k,v in pairs(lua1) do
+    print(k,v)
+end
