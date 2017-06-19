2cdeb8304c86f027ff812c2a8dcd11c5df7d95f3
heyuanlong <1023417614@qq.com>
2017-06-19 11:05:30 +0800

    lua except

--- __lua/syntaxTest/exception.lua
+++ __lua/syntaxTest/exception.lua
@@ -1,3 +1,8 @@
+
+-- 某些lua版本的xpcall不会传参数。
+
+
+
 local status, msgId, req = pcall(function()
                 assert(true, "errormsg")
                 return 1000, "req"
@@ -16,3 +21,36 @@ local status, msgId, req = pcall(function()
             end)
 print(status,msgId,req)						--false   exception.lua:14: errormsg      nil
 
+
+
+function square(a)
+      return a * "a"
+end
+function square1(a)
+      return a * a
+end
+
+print()
+local status, retval = pcall(square,10);
+print ("Status: ", status)        -- 打印 "false" 
+print ("Return Value: ", retval)
+
+
+print("----------------------------")
+status,  ret = xpcall(square, debug.traceback, 10)
+print ("status: ", status)        -- 打印 "false" 
+print ("ret: ", ret)
+
+print("----------------------------")
+status,  ret = xpcall(square, function(errormsg) print(debug.traceback(errormsg)) end, 10)
+print ("status: ", status)        -- 打印 "false" 
+print ("ret: ", ret)
+
+print("+++++++++++++++++++++++++++++")
+status,  ret = xpcall(square1, function(errormsg) print(errormsg) end, 10)
+print ("status: ", status)        -- 打印 "false" 
+print ("ret: ", ret)
+
+
+print("+++++++++++++++++++++++++++++")
+xpcall(function(i) print(i) error('error..') end, function() print(debug.traceback()) end, 33)
